ab5379da3e11f311346bb76c1f5956c75bda76a6
swarajpidurkar <pidurkarswarj@gmail.com>
2023-04-07 16:44:35 +0530

    completed CategoryRepository

---
+++ src/main/java/com/bikkadit/electronic/store/ElectronicStore/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.bikkadit.electronic.store.ElectronicStore.repositories;
+
+import com.bikkadit.electronic.store.ElectronicStore.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,String> {
+
+
+}
