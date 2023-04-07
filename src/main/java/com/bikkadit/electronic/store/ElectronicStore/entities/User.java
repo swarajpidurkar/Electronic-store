eabd8bc0f96a01030ffbbfa31aaa1f2e21f1ad9f
swarajpidurkar <pidurkarswarj@gmail.com>
2023-04-07 11:05:28 +0530

    completed user entity

---
+++ src/main/java/com/bikkadit/electronic/store/ElectronicStore/entities/User.java
@@ -0,0 +1,43 @@
+package com.bikkadit.electronic.store.ElectronicStore.entities;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+
+    @Id
+    private String userId;
+
+    @Column(name="user_name")
+    private String name;
+
+    @Column(name="user_email",unique = true)
+    private String email;
+
+    @Column(name="user_password",length = 10)
+    private String password;
+
+
+    private String gender;
+
+    @Column(length= 1000)
+    private String about;
+
+    @Column(name="user_image_name")
+    private String imageName;
+}
+
